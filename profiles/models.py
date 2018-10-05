91d5df8872249f2f8d1326788865b04cf24f8d71
claudianjeri <claudianjeri04@gmail.com>
2018-10-05 19:37:18 +0300

    added gitignore file and prepared for static

--- profiles/models.py
+++ profiles/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 class Profiles(models.Model):
     name=models.CharField(max_length=120)
-    description =models.TextField(null=True)
+    email =models.CharField(max_length=40, blank=False, default='my email default')
     location = models.CharField(max_length=40, default='my location default')
+    
     def __unicode__(self):
         return self.name
