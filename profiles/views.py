122187194d1c41237dcdf1bb882034f26784a79b
claudianjeri <claudianjeri04@gmail.com>
2018-10-06 13:54:42 +0300

    added navbar and footer

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     context = locals()
-    template = 'home.html'
+    template = 'base.html'
 
     return render(request, template, context)
