3116bcb9325198467da091e6489a0858a3d7f89b
claudianjeri <claudianjeri04@gmail.com>
2018-10-07 11:32:25 +0300

    added and modified about us

--- aramati/urls.py
+++ aramati/urls.py
@@ -15,17 +15,18 @@ Including another URLconf
 """
 from django.conf import settings 
 from django.conf.urls.static import static
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 from profiles import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home, name='home')
+    url('', include('profiles.urls'))
 
 
 ]
+
 
 # if settings.DEBUG:
 #     urlpatterns += static(settings,STATIC_URL, document_root=settings.STATIC_ROOT)
--- profiles/views.py
+++ profiles/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    context = locals()
-    template = 'base.html'
+   context = locals()
+   template = 'home.html'
 
-    return render(request, template, context)
+   return render(request, template, context)
 
-def home(request):
-  return render(request, 'home.html')
+# def home(request):
+#   return render(request, 'home.html')
+
+def aboutus(request):
+   return render(request, 'about.html') 
