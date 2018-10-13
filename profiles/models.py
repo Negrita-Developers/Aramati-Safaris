4ab0f028d3ba2e76b73299a103f239f940b42869
claudianjeri <claudianjeri04@gmail.com>
2018-10-13 15:11:11 +0300

    added buttons to index page and a whole other stuff i forgot to add

--- profiles/models.py
+++ profiles/models.py
@@ -8,3 +8,15 @@ class Profiles(models.Model):
     
     def __unicode__(self):
         return self.name
+
+class SpecialOffers(models.Model):
+    title=models.CharField(max_length=120)
+    depature=models.DateField(auto_now_add=True)
+    # images=
+    # bookbefore=
+    # duration=
+    # accomodation=
+    # itinerary=
+    # included=
+    # notincluded=
+
