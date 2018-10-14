86868adde330a87450e44ec9507eb36b3d42f700
claudianjeri <claudianjeri04@gmail.com>
2018-10-14 20:08:00 +0300

    added excursions page

--- profiles/views.py
+++ profiles/views.py
@@ -2,13 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-   context = locals()
-   template = 'home.html'
-
-   return render(request, template, context)
-
-# def home(request):
-#   return render(request, 'home.html')
+   return render(request, 'home.html') 
 
 def aboutus(request):
    return render(request, 'about.html') 
--- tours/views.py
+++ tours/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def excursions(request):
+   return render(request, 'excursions.html') 
