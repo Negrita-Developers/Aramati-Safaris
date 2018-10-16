c01acb7a9497522af957eba4f668a8fe77d29081
claudianjeri <claudianjeri04@gmail.com>
2018-10-16 12:30:46 +0300

    removed dropdown

--- aramati/settings.py
+++ aramati/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'aramati',
         'USER': 'claudia',
-    'PASSWORD':'ino ni momo4',
+    'PASSWORD':'claudia04',
     }
 }
 
