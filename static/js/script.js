0cabd38a7a4c32579504febb412e5a2f542639d0
claudianjeri <claudianjeri04@gmail.com>
2018-11-01 14:54:13 +0300

    modified about us page

--- static/js/script.js
+++ static/js/script.js
@@ -1107,4 +1107,28 @@
     
   });	
 
+  $("html").mousemove(function(e) {
+  
+    var deg = (e.pageY/10)+240;
+    var rotation = 'rotateX(' + deg + 'deg)'; 
+    var rotate = {
+        '-webkit-transform' : rotation,
+        '-moz-perspective' : rotation,
+        '-ms-perspective' : rotation,
+        'perspective' : rotation
+      }
+    $("#shadow").css(rotate);
+        
+        
+    var perspective = (e.pageX/2) + 25;
+    var perspective = {
+        '-webkit-perspective' : perspective,
+        '-moz-perspective' : perspective,
+        '-ms-perspective' : perspective,
+        'perspective' : perspective
+      }
+    $("#perspective").css(perspective);
+    
+  });
+
 })();
