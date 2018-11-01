6cc10f876a328360496faf5cf168d50c5e3cc1c4
claudianjeri <claudianjeri04@gmail.com>
2018-11-01 07:54:37 +0300

    changed settings

--- aramati/settings.py
+++ aramati/settings.py
@@ -127,8 +127,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
