8aea9ed6198940bfb1997d90fceeb2db9c084967
claudianjeri <claudianjeri04@gmail.com>
2018-11-09 13:23:44 +0300

    added views of the urls...daaamn

--- profiles/urls.py
+++ profiles/urls.py
@@ -9,41 +9,55 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^about/$', views.aboutus, name='aboutus'),
     url(r'^contactus/$', views.contact, name='contact'),
-    # url(r'^joinsafarigroups/$', views.joingroup, name='joingroup'),
     url(r'^guaranteedsafaris/$', views.guaranteedsafaris, name='guaranteedsafaris'),
-    # url(r'^singlegroup/(?P<title>\d+)', views.single, name='single'),
     url(r'^excursions/$', views.excursions, name='excursions'),
     url(r'^longsafaris/$', views.longsafaris, name='longsafaris'),
     url(r'^shortsafaris/$', views.shortsafaris, name='shortsafaris'),
+
     url(r'^lakenaivashapackage/$', views.lakenaivasha, name='lakenaivasha'),
     url(r'^joinlakenaivashapackage/$', views.joinlakenaivasha, name='joinlakenaivasha'),
+
     url(r'^lakenakurupackage/$', views.lakenakuru, name='lakenakuru'),
     url(r'^joinlakenakurupackage/$', views.joinlakenakuru, name='joinlakenakuru'),
+    
     url(r'^nairobiexcursionpackage/$', views.nairobiexcursion, name='nairobiexcursion'),
+
     url(r'^shortsafarislakenakuru/$', views.ssfnakuru, name='ssfnakuru'),
     url(r'^joinshortsafarislakenakuru/$', views.joinssfnakuru, name='joinssfnakuru'),
+
     url(r'^shortsafarisaberdare/$', views.aberdaressf, name='aberdaressf'),
     url(r'^joinshortsafarisaberdare/$', views.aberdarejoinssf, name='aberdarejoinssf'),
+
     url(r'^shortsafarisamboseli/$', views.amboselissf, name='amboselissf'), 
     url(r'^joinshortsafarisamboseli/$', views.amboselijoinssf, name='amboselijoinssf'),
+
     url(r'^shortsafarislakenaivasha/$', views.naivashassf, name='naivashassf'), 
     url(r'^joinshortsafarislakenaivasha/$', views.naivashajoinssf, name='naivashajoinssf'),
+
     url(r'^shortsafarismaasaimara/$', views.maasaimarassf, name='maasaimarassf'),
     url(r'^joinshortsafarismaasaimara/$', views.maasaimarajoinssf, name='maasaimarajoinssf'),
+
     url(r'^shortsafarissweetwaters/$', views.pajetassf, name='pajetassf'),
     url(r'^joinshortsafarissweetwaters/$', views.pajetajoinssf, name='pajetajoinssf'),
+
     url(r'^shortsafarisamboseli3days/$', views.amboselissf3, name='amboselissf3'),
     url(r'^joinshortsafarisamboseli3days/$', views.amboselijoinssf3, name='amboselijoinssf3'),
+
     url(r'^shortsafarissweetwater3days/$', views.sweetwaterssf3, name='sweetwaterssf3'),
     url(r'^joinshortsafarissweetwater3days/$', views.sweetwaterjoinssf3, name='sweetwaterjoinssf3'),
+
     url(r'^shortsafarismaasaimara3days/$', views.maasaimarassf3, name='maasaimarassf3'),
     url(r'^joinshortsafarismaasaimara3days/$', views.maasaimarajoinssf3, name='maasaimarajoinssf3'),
+
     url(r'^shortsafarisnakuru3days/$', views.nakurussf3, name='nakurussf3'),
     url(r'^joinshortsafarisnakuru3days/$', views.nakurujoinssf3, name='nakurujoinssf3'),
+
     url(r'^shortsafarissamburu3days/$', views.samburussf3, name='samburussf3'),
     url(r'^joinshortsafarissamburu3days/$', views.samburujoinssf3, name='samburujoinssf3'),
+
     url(r'^shortsafarismaasarimaranakuru4days/$', views.maranakurussf4, name='maranakurussf4'),
     url(r'^joinshortsafarismaasarimaranakuru4days/$', views.maranakurujoinssf4, name='maranakurujoinssf4'),
+
     url(r'^shortsafarisamboseli5days/$', views.amboselissf5, name='amboselissf5'),
     url(r'^joinshortsafarisamboseli5days/$', views.amboselijoinssf5, name='amboselijoinssf5'),
 ] 
--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Packages , LowSeason, JoinedSafaris , HighSeason, PeakSeason
 
 # Create your views here.
+
+# General view pages
 def home(request):
    return render(request, 'home.html') 
 
@@ -11,36 +13,42 @@ def aboutus(request):
 def contact(request):
    return render(request, 'contacts.html') 
 
-def guaranteedsafaris(request):
-
-    guaras = JoinedSafaris.objects.all()
-    single = Packages.objects.all()
-    
-    return render(request, 'joingroup/joining.html', {"guaras": guaras}, {" single":  single})
-
-def bookings(request):
-    return render(request, 'bookings/booking-traveler.html')  
-
+#excursions
 def excursions (request):
     return render(request, 'excursions/excursions.html')
 
 def lakenaivasha (request):
     return render(request, 'excursions/lakenaivasha.html')
-    
+
+def joinlakenaivasha (request):
+    guaras = JoinedSafaris.objects.all()
+    single = Packages.objects.all()
+    return render(request, 'joingroup/excursions/lakenaivasha.html', {"guaras": guaras}, {" single":  single})
+
 def lakenakuru (request):
-    return render(request, 'excursions/lakenakuru.html') 
+    return render(request, 'excursions/lakenakuru.html')  
+
+def joinlakenakuru (request):
+    guaras = JoinedSafaris.objects.all()
+    single = Packages.objects.all()
+    return render(request, 'joingroup/excursions/lakenakuru.html', {"guaras": guaras}, {" single":  single})    
 
 def nairobiexcursion (request):
     return render(request, 'excursions/nairobiexcursions.html')
+ 
 
-def longsafaris(request):
-    return render(request, 'longsafaris/longsafaris.html')
+#short safaris
 
 def shortsafaris (request):
     return render(request, 'shortsafaris/shortsafaris.html')
 
 def ssfnakuru(request):
     return render (request, 'shortsafaris/2days/lakenakuru.html')
+
+def joinssfnakuru(request):
+    guaras = JoinedSafaris.objects.all()
+    single = Packages.objects.all()
+    return render (request, 'joingroup/2days/lakenakuru.html', {"guaras": guaras}, {" single":  single})
 
 def aberdaressf(request):
     return render (request, 'shortsafaris/2days/aberdare.html')
@@ -50,39 +58,106 @@ def aberdarejoinssf(request):
     single = Packages.objects.all()
     return render (request, 'joingroup/2days/aberdare.html', {"guaras": guaras}, {" single":  single})
 
-def  amboselissf(request):
+def amboselissf(request):
     return render (request, 'shortsafaris/2days/amboseli.html') 
+
+def amboselijoinssf(request):
+    guaras = JoinedSafaris.objects.all()
+    single = Packages.objects.all()
+    return render (request, 'joingroup/2days/amboseli.html', {"guaras": guaras}, {" single":  single}) 
 
 def naivashassf(request):
     return render (request, 'shortsafaris/2days/naivasha.html')
 
+def naivashajoinssf(request):
+    guaras = JoinedSafaris.objects.all()
+    single = Packages.objects.all()
+    return render (request, 'joingroup/2days/naivasha.html', {"guaras": guaras}, {" single":  single})
+
 def maasaimarassf(request):
     return render (request, 'shortsafaris/2days/maasaimara.html')
+
+def maasaimarajoinssf(request):
+    guaras = JoinedSafaris.objects.all()
+    single = Packages.objects.all()
+    return render (request, 'joingroup/2days/maasaimara.html', {"guaras": guaras}, {" single":  single})
 
 def pajetassf(request):
     return render (request, 'shortsafaris/2days/sweetwaters.html')
 
+def pajetajoinssf(request):
+    guaras = JoinedSafaris.objects.all()
+    single = Packages.objects.all()
+    return render (request, 'joingroup/2days/sweetwaters.html', {"guaras": guaras}, {" single":  single})
+
 def amboselissf3(request):
     return render (request, 'shortsafaris/3days/amboseli.html') 
+
+def amboselijoinssf3(request):
+    guaras = JoinedSafaris.objects.all()
+    single = Packages.objects.all()
+    return render (request, 'joingroup/3days/amboseli.html', {"guaras": guaras}, {"single":  single})
 
 def sweetwaterssf3(request):
     return render (request, 'shortsafaris/3days/sweetwaters.html') 
 
+def sweetwaterjoinssf3(request):
+    guaras = JoinedSafaris.objects.all()
+    single = Packages.objects.all()
+    return render (request, 'joingroup/3days/sweetwaters.html', {"guaras": guaras}, {"single":  single})
+
 def maasaimarassf3(request):
     return render (request, 'shortsafaris/3days/maasaimara.html') 
+
+def maasaimarajoinssf3(request):
+    guaras = JoinedSafaris.objects.all()
+    single = Packages.objects.all()
+    return render (request, 'joingroup/3days/maasaimara.html', {"guaras": guaras}, {"single":  single})
 
 def nakurussf3(request):
     return render (request, 'shortsafaris/3days/nakuru.html') 
 
+def nakurujoinssf3(request):
+    guaras = JoinedSafaris.objects.all()
+    single = Packages.objects.all()
+    return render (request, 'joingroup/3days/nakuru.html', {"guaras": guaras}, {"single":  single})
+
 def samburussf3(request):
     return render (request, 'shortsafaris/3days/samburu.html') 
+
+def samburujoinssf3(request):
+    guaras = JoinedSafaris.objects.all()
+    single = Packages.objects.all()
+    return render (request, 'joingroup/3days/samburu.html', {"guaras": guaras}, {"single":  single})
 
 def maranakurussf4(request):
     return render (request, 'shortsafaris/4days/mara-nakuru.html') 
 
+def maranakurujoinssf4(request):
+    guaras = JoinedSafaris.objects.all()
+    single = Packages.objects.all()
+    return render (request, 'joingroup/4days/mara-nakuru.html', {"guaras": guaras}, {"single":  single}) 
+
 def amboselissf5(request):
     return render (request, 'shortsafaris/5days/amboseli.html') 
 
+def amboselijoinssf5(request):
+    guaras = JoinedSafaris.objects.all()
+    single = Packages.objects.all()
+    return render (request, 'joingroup/5days/amboseli.html', {"guaras": guaras}, {"single":  single}) 
 
+#long safaris
+def longsafaris(request):
+    return render(request, 'longsafaris/longsafaris.html')
 
+#joining groups
+def guaranteedsafaris(request):
 
+    guaras = JoinedSafaris.objects.all()
+    single = Packages.objects.all()
+    
+    return render(request, 'joingroup/joining.html', {"guaras": guaras}, {" single":  single})
+
+#booking and checkout
+def bookings(request):
+    return render(request, 'bookings/booking-traveler.html') 
